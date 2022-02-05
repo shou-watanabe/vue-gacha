2ab62d90c29b07f9002454287ec4a9c87c47125d
shou-watanabe <watanabeshou1520@gmail.com>
2022-02-05 13:29:53 +0900

    :memo: AppBarのコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/slime.png"
-          transition="scale-transition"
-          width="40"
-        />
-        <div>Gopher Quest</div>
-
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <v-icon>mdi-wrench</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
---
+++ src/components/commons/AppBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="@/assets/slime.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <div>Gopher Quest</div>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <v-icon>mdi-wrench</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppBar",
+});
+</script>
