7753eb56d06238aff32967f1a0c4ef811a4b9bba
shou-watanabe <watanabeshou1520@gmail.com>
2022-02-05 21:38:29 +0900

    :sparkles: Gacha画面実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,27 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/HomePage.vue";
+import Login from "../views/LoginPage.vue";
+import Gacha from "../views/GachaPage.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
     path: "/",
     name: "Home",
     component: Home,
   },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
+  {
+    path: "/gacha",
+    name: "Gacha",
+    component: Gacha,
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/GachaPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <AppBar />
+    <v-container>
+      <v-row justify="center" class="align-center">
+        <v-text-field
+          label="回数"
+          :style="{ maxWidth: '200px' }"
+          v-model="times"
+          type="number"
+          suffix="回"
+        />
+        <v-btn @click="gachaDraw">ガチャる</v-btn>
+      </v-row>
+      <v-row justify="center" class="align-center">
+        <div v-if="items.length !== 0">
+          <CharaTable :items="items" />
+        </div>
+      </v-row>
+      <v-row justify="center" class="align-center">
+        <v-btn @click="moveHome">ホームに戻る</v-btn>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios";
+import AppBar from "@/components/commons/AppBar.vue";
+import CharaTable from "@/components/commons/CharaTable.vue";
+import { Item } from "@/types/item";
+
+export default Vue.extend({
+  name: "GachaPage",
+  components: {
+    AppBar,
+    CharaTable,
+  },
+  computed: {
+    userToken(): string {
+      return this.$store.state.token;
+    },
+  },
+  data() {
+    return {
+      items: [] as Array<Item>,
+      times: "",
+    };
+  },
+  methods: {
+    gachaDraw(): void {
+      const useAxios = axios.create({
+        auth: {
+          username: "1",
+          password: "pass",
+        },
+        headers: {
+          "x-token": this.userToken,
+        },
+      });
+      useAxios
+        .post("/gacha/draw", { times: parseInt(this.times) })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data["results"] != null) {
+            this.makeTableItems(res.data["results"]);
+          }
+        });
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    makeTableItems(res: Array<any>): void {
+      this.items = res.map((item) => {
+        return {
+          name: item["name"],
+          icon: item["icon_url"],
+          rarity: item["rarity"],
+        } as Item;
+      });
+    },
+    moveHome(): void {
+      this.$router.push({ name: "Home" });
+    },
+  },
+});
+</script>
